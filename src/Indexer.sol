13dfccb163b4a68e7844c1bf471c1bf2a7778fb8
nicoacosta.eth <nicoacosta8@gmail.com>
2024-08-24 03:38:45 -0300

    initial commit

---
+++ src/Indexer.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+enum TransactionType {
+    NULL,
+    DEPOSIT,
+    WITHDRAW
+}
+
+struct Transaction {
+    TransactionType _type;
+    uint256 amount;
+    uint256 timestamp;
+    string cid;
+}
+
+event TransactionAdded(
+        TransactionType _type,
+        uint256 amount,
+        uint256 timestamp,
+        string cid
+    );
+
+contract Indexer is Ownable {
+    Transaction[] public transactions;
+
+    constructor(address _owner) Owner(_owner) {}
+
+    function addTransaction(
+        TransactionType _type,
+        uint256 _amount,
+        uint256 _timestamp,
+        string memory _cid
+    ) public onlyOwner {
+        transactions.push(Transaction(_type, _amount, _timestamp, _cid));
+
+        emit TransactionAdded(_type, _amount, _timestamp, _cid);
+    }
+}
