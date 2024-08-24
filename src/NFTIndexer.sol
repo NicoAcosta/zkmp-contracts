02628235005dafc283d99cc4e5d8ffe5738e9fc4
nicoacosta.eth <nicoacosta8@gmail.com>
2024-08-24 15:10:51 -0300

    nft ready

--- src/NFTIndexer.sol
+++ src/NFTIndexer.sol
@@ -2,15 +2,17 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract Indexer is ERC721URIStorage, Ownable {
-    address public holder;
-
     uint256 private _tokenId;
 
-    constructor() ERC721("OpenFlow", "OF") Ownable(msg.sender) {}
+    address public holder;
+
+    constructor(
+        address _owner,
+        address _holder
+    ) ERC721("OpenFlow", "OF") Ownable(msg.sender) {}
 
     function mint(string memory _uri) public onlyOwner {
         __mint(_uri);
